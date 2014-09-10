6f049716508086353475fc8cedcb2317abb47686
Emily Yeh <emilyyeh@cmu.edu>
2014-09-09 23:08:54 -0400

    Added gameplay pages

--- MAS/js/experiment.js
+++ MAS/js/experiment.js
@@ -1,7 +1,141 @@
-var agencyConditions = ["low", "high"]; //should we add a none?
-var gameConditions = ["Rel", "Irr", "All", "None"];
+var agencyConditions = ["low", "high"];
+var gameConditions = ["Rel", "Irr", "Both", "Neither"];
+var activities = [0,1,2,3];
+if(testing) { activities = [0,0,0,0]; }
+/* Activities
+** 0: Rel1
+** 1: Rel2
+** 2: Irr1
+** 3: Irr2
+*/
+
+var randomPick = function (list) {
+	if (list.length == 0) { return null; }
+	else {
+		return (list[Math.floor(Math.random() * list.length)]);
+	}
+};
+
+var randomShuffle = function (list) {
+	//Fisher-Yates (Knuth) Shuffle
+	var currentIndex = list.length, temporaryValue, randomIndex;
+
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		temporaryValue = list[currentIndex];
+		list[currentIndex] = list[randomIndex];
+		list[randomIndex] = temporaryValue;
+	}
+
+	return list;
+};
 
 var condition = {
-  agency: agencyConditions[Math.floor(Math.random() * 2)],
-  game: gameConditions[Math.floor(Math.random() * 4)]
+  agency: randomPick(agencyConditions),
+  game: randomPick(gameConditions),
+  activityOrder: randomShuffle(activities)
+};
+
+var results = {
+	rel1: [],
+	rel2: [],
+	irr1: [],
+	irr2: [],
+	prescreen: [],
+	
+};
+var clearScreen2 = function(nextScreen, stage, id, trialsLeft) {
+  $('#page-title').empty();
+  $('#content-instructions').empty();
+  $('#content-text').empty();
+  if (nextScreen < 100) {
+    document.getElementById('button-next').onclick = function() { next(nextScreen) };
+  } else if (nextScreen < 200) {
+    document.getElementById('button-next').onclick = function() { runActivity(nextScreen, stage, id, trialsLeft) };
+  }
+};
+
+var gameInstructions = function(stage) {
+	showI();
+	document.getElementById('page-title').innerHTML = "<h1>INSTRUCTIONS</h1>";
+	var s = '<object type="text/html" style="width:700px;height:120px;margin:0;" data="inst'+stage.toString()+'.html" ></object>';
+	document.getElementById('content-instructions').innerHTML = s;
+};
+
+var practicePrep = function(stage) {
+	showI();
+	document.getElementById('page-title').innerHTML = "<h1>PRACTICE ROUND</h1>";
+    document.getElementById('content-instructions').innerHTML = '<object type="text/html" style="width:700px;margin:-11px;" data="practicePrep.html" ></object>';
+};
+var gameplay = function(stage, trial) {
+	console.log(trial);
+	//do gameplay stuff here
+	//return result
+	//go back to case 104
+};
+var scoredPrep = function(stage) {
+	showI();
+	document.getElementById('page-title').innerHTML = "<h1>SCORED ROUNDS</h1>";
+    document.getElementById('content-instructions').innerHTML = '<object type="text/html" style="width:700px;margin:-20px;" data="scoredPrep.html" ></object>';
+};
+var scoredResult = function(stage) {
+	showI();
+	document.getElementById('page-title').innerHTML = "<h1>ROUND RESULTS</h1>";
+    document.getElementById('content-instructions').innerHTML = '<object type="text/html" style="width:700px;margin:-20px;" data="scoredPrep.html" ></object>';
+};
+var pDilemma = function(stage) {
+	showI();
+	document.getElementById('page-title').innerHTML = "<h1>ROUND RESULTS</h1>";
+    document.getElementById('content-instructions').innerHTML = '<object type="text/html" style="width:700px;margin:-20px;" data="scoredPrep.html" ></object>';
+};
+
+var runActivity = function (page, stage, id, trialsLeft) {
+	/* Page: which page in the activity it is
+	** stage: which game it is
+	** id: how many games you've done + 1 (which game youre on)
+	** trialsLeft: trials left 0 < x < 11
+	*/
+	var trials = 10-trialsLeft; // 0 <= x < 10
+	switch (page) {
+		case 100: //game instructions
+			clearScreen2(101, stage, id, trialsLeft);
+			gameInstructions(stage);
+			break;
+		case 101: //practice round prep
+			clearScreen2(102, stage, id, trialsLeft);
+			practicePrep(stage);
+			break;
+		case 102: //practice round
+			clearScreen2(103, stage, id, trialsLeft);
+			gameplay(stage, trials);
+			break;
+		case 103: //scored rounds prep
+			clearScreen2(104, stage, id, trialsLeft);
+			scoredPrep(stage);
+			break;
+		case 104: //scored rounds process
+			if (trialsLeft > 0) {
+				trialsLeft -= 1;
+				clearScreen2(104, stage, id, trialsLeft);
+				gameplay(stage, trials);
+			}
+			else {
+				console.log("Finished trials");
+				runActivity(105, stage, id, trialsLeft);
+			}
+			break;
+		case 105: //summary of rounds and pdilemma
+			clearScreen2(106, stage, id, trialsLeft);
+			console.log("old id is "+id);
+			console.log("pdilemma!");
+			pDilemma(stage);
+			break;
+		case 106: //end
+			console.log("done!!");
+			id += 1;
+			runActivities(id);
+			break;
+	}
 };
--- MAS/js/index.js
+++ MAS/js/index.js
@@ -1,43 +1,67 @@
 var $;
 var Handlebars;
+var testing = true;
 
 var clearScreen = function(nextScreen) {
   $('#page-title').empty();
   $('#content-instructions').empty();
-  $('#content-instructions').hide();  
   $('#content-text').empty();
-  document.getElementById('button-next').onclick = function() { next(nextScreen) };
+  if (nextScreen < 100) {
+    document.getElementById('button-next').onclick = function() { next(nextScreen) };
+  }
+};
+
+var showI = function () {
+  $('#content-instructions').show();  
+  $('#content-text').hide();
+}
+var showT = function () {
+  $('#content-instructions').hide();  
+  $('#content-text').show();
+}
+
+var runHelper = function(id) {
+  currActivity = condition.activityOrder[id];
+  var done = runActivity(100, currActivity);
+}
+
+var runActivities = function(id) {
+  console.log("starting activity " + id);
+  if (id > 3) { return true; }
+  currActivity = condition.activityOrder[id];
+  runActivity(100, currActivity, id, 10);
 };
 
 var next = function(id) {
     switch(id) {
       case 0: // Display consent screen
         clearScreen(1);
-        document.getElementById('content-text').innerHTML = '<object type="text/html" style="width:700px;height:500px" data="consent.html" ></object>';
+        showT();
+        document.getElementById('content-text').innerHTML = '<object type="text/html" style="width:700px;height:450px" data="consent.html" ></object>';
         break;
       case 1: // Display prescreen
         clearScreen(2);
         document.getElementById('page-title').innerHTML = "<h1>PRE-SCREEN SURVEY</h1>"
-        document.getElementById('content-text').innerHTML = '<object type="text/html" style="width:700px;height:500px" data="prescreen.html" ></object>';
+        document.getElementById('content-text').innerHTML = '<object type="text/html" style="width:700px;height:400px" data="prescreen.html" ></object>';
         break;
       case 2: // Display loading page
         clearScreen(3);
-        $('#content-instructions').show();
+        showI();
         $('#button-next').hide();
         document.getElementById('content-instructions').innerHTML = '<object type="text/html" style="width:700px;" data="postPrescreen.html" ></object>';
-        setTimeout(function() { next(3) }, 2000);
+        if(testing) { next(3); }
+        else { (function() { next(3) }, 2000); }
         break;
       case 3: // Display pre-screen results screen
         clearScreen(4);
         $('#button-next').show();
-        $('#content-instructions').show();
         document.getElementById('page-title').innerHTML = "<h1>PRE-SCREEN RESULTS</h1>"
         switch (condition.agency) {
           case "high":
-            document.getElementById('content-instructions').innerHTML = '<object type="text/html" style="width:700px;" data="groupPlacementHigh.html" ></object>';
+            document.getElementById('content-instructions').innerHTML = '<object type="text/html" style="width:700px;height:120px;margin:0;" data="groupPlacementHigh.html" ></object>';
             break; 
           case "low":
-            document.getElementById('content-instructions').innerHTML = '<object type="text/html" style="width:700px;" data="groupPlacementLow.html" ></object>';
+            document.getElementById('content-instructions').innerHTML = '<object type="text/html" style="width:700px;height:120px;margin:0;" data="groupPlacementLow.html" ></object>';
             break;
           default:
             console.log("404: Agency category not found.");
@@ -46,17 +70,17 @@ var next = function(id) {
       case 4: // Display group placement intro screen
         clearScreen(5);
         document.getElementById('page-title').innerHTML = "<h1>GROUP ASSIGNMENT</h1>" //is this the right word? look up
-        document.getElementById('content-text').innerHTML = '<object type="text/html" style="width:700px;height:500px" data="groupIntro.html" ></object>';
+        document.getElementById('content-instructions').innerHTML = '<object type="text/html" style="width:700px;height:140px;margin-top:-20px;" data="groupIntro.html" ></object>';
         break;
       case 5: // Display members info screen
         clearScreen(6);
+        showT();
         document.getElementById('page-title').innerHTML = "<h1>GROUP ASSIGNMENT</h1>" //is this the right word? look up
-        $('#content-instructions').hide();
-        document.getElementById('content-text').innerHTML = '<object type="text/html" style="width:700px;height:500px" data="groupInfo.html" ></object>';
+        document.getElementById('content-text').innerHTML = '<object type="text/html" style="width:700px;height:400px" data="groupInfo.html" ></object>';
         break;
       case 6: // should a group cohesion task go here?
-        clearScreen(7);
-        document.getElementById('content-text').innerHTML = '<h1>Group cohesion task?</h1>';
+        runActivities(0);
+        break;
       case 7:
         //experiments go here!
       default:
